eafaf4f3922492b2ef8c85e5a8384a40c6a423cc
Mike Bild <mike.bild@googlemail.com>
2016-07-24 16:51:37 +0200

    #4 library usage and integration tests

--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -1,19 +1,104 @@
 const fs = require('fs');
 const assert = require('assert');
-const join = require('path').join;
+const path = require('path');
 
-describe('integration first', function() {
-  it('should work on ', function() {
-    const schema = read('first.graphql');
-    const expected = json('first.json');
-    assert.deepEqual(1, 1);
-  });
-});
+const PouchDB = require('../../lib/pouch-graphql/pouchdb');
+const graphql = require('graphql').graphql;
+const graphqlPouchSchema = require('../../lib/pouch-graphql');
 
-function read (path) {
-  return fs.readFileSync(join(__dirname, 'fixtures', path), 'utf8');
+const ENVIRONMENT = 'test';
+const TEST_FIXTURES = 'test/integration/fixtures';
+const SCHEMA_DEFINITION = `
+type Post {
+  id: ID
+  rev: String
+  personId: ID
+  title: String
+  body: String
 }
 
-function json (path) {
-  return require(join(__dirname, 'fixtures', path));
+type Comment {
+  id: ID
+  rev: String
+  personId: ID
+  title: String
+}
+
+type Person {
+  id: ID
+  rev: String
+  name: String
+}
+
+type Tag {
+  id: ID
+  value: String
+}
+`;
+
+describe('Integration tests', function() {
+  const db = PouchDB.createPouchDB(ENVIRONMENT);
+
+  before(() => {
+    const post1 = {
+      doctype: 'Post',
+      _id: 'test1',
+      title: 'title 1',
+      body: 'body 1',
+    };
+    const post2 = {
+      doctype: 'Post',
+      _id: 'test2',
+      title: 'title 2',
+      body: 'body 2',
+    };
+    const post3 = {
+      doctype: 'Post',
+      _id: 'test3',
+      title: 'title 3',
+      body: 'body 3',
+    };
+
+    return db.bulkDocs([
+      post1,
+      post2,
+      post3,
+    ]);
+  });
+
+  after(() => {
+    return db.destroy();
+  });
+
+  fs.readdirSync(TEST_FIXTURES).forEach(fileName => {
+    if (path.extname(fileName) === '.graphql') {
+      const name = path.basename(fileName, '.graphql');
+
+      it(`GraphQL query ${name}`, () => {
+        const sut = graphqlPouchSchema(ENVIRONMENT, SCHEMA_DEFINITION, false, []);
+        const expectedData = json(`${name}.json`);
+
+        const operationName = null;
+        const rootValue = null;
+        const contextValue = {environment:ENVIRONMENT, user: {}};
+        const schemaQuery = read(`${name}.graphql`);
+        const variableValues = null;
+
+        return sut.query(schemaQuery, variableValues, rootValue, contextValue, operationName)
+          .then(result => {
+            assert.deepEqual(result, expectedData)
+          });
+      });
+
+    }
+  });
+
+});
+
+function read (filePath) {
+  return fs.readFileSync(path.join(__dirname, 'fixtures', filePath), 'utf8');
+}
+
+function json (filePath) {
+  return require(path.join(__dirname, 'fixtures', filePath));
 }
